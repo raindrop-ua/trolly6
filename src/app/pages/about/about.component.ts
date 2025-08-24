901f17d04aff9982f9311a7fe85f9dca24edd3dc
Anton Sizov <raindropua@gmail.com>
2025-08-24 15:36:34 +0300

    chore: integrate section components into the About page template, replace static HTML with reusable components

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeadComponent } from '@pages/about/sections/head/head.component';
+import { OverviewComponent } from '@pages/about/sections/overview/overview.component';
+import { KeyFactsComponent } from '@pages/about/sections/key-facts/key-facts.component';
+import { InfrastructureComponent } from '@pages/about/sections/infrastructure/infrastructure.component';
+import { MediaComponent } from '@pages/about/sections/media/media.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    HeadComponent,
+    OverviewComponent,
+    KeyFactsComponent,
+    InfrastructureComponent,
+    MediaComponent
+  ],
   templateUrl: './about.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
