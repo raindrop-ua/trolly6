597103174302a5ea33ffe7e3a80e432044634e6a
Anton Sizov <raindropua@gmail.com>
2025-08-24 11:31:28 +0300

    chore: refactor Home, Schedule, and PublicLayout components by modularizing UI sections and updating templates

--- src/app/core/layouts/public-layout/public-layout.component.ts
+++ src/app/core/layouts/public-layout/public-layout.component.ts
@@ -1,13 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-public-layout',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './public-layout.component.html',
-  styleUrl: './public-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PublicLayoutComponent {
