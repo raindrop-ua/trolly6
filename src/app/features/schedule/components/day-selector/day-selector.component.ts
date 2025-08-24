af8b8344be0bef0281f441c23d3ceeef7ef00e05
Anton Sizov <raindropua@gmail.com>
2025-08-24 14:59:41 +0300

    chore: create DaySelectorComponent with template and OnPush change detection strategy

---
+++ src/app/features/schedule/components/day-selector/day-selector.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-selector',
+  imports: [],
+  templateUrl: './day-selector.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DaySelectorComponent {
+
+}
