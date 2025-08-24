d6c67c204950db6684deb21d5c5104a50212fc80
Anton Sizov <raindropua@gmail.com>
2025-08-24 09:25:24 +0300

    chore: add ThemeService and ThemeSwitcherComponent with theme toggling functionality and UI

---
+++ src/app/pages/concepts/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ThemeService } from '../../../core/services/theme.service';
+
+@Component({
+  selector: 'app-theme-switcher',
+  imports: [],
+  templateUrl: './theme-switcher.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ThemeSwitcherComponent {
+  private readonly theme = inject(ThemeService);
+
+  themeSignal = this.theme.theme;
+
+  toggleTheme() {
+    this.theme.toggle();
+  }
+}
