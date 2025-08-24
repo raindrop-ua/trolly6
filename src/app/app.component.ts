5cf8847ed5117899c17c7b1806dbbd9f8531cbb8
Anton Sizov <raindropua@gmail.com>
2025-08-24 09:24:44 +0300

    chore: add Concept1Component to AppComponent and update the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Concept1Component} from './pages/concepts/concept1/concept1.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Concept1Component],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
