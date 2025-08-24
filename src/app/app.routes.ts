8401f4412e9875476314f020e8771c5f053be0e8
Anton Sizov <raindropua@gmail.com>
2025-08-24 11:32:01 +0300

    chore: configure custom path aliases in tsconfig, enhance app initialization with SeoService, and update routes with SEO metadata

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,40 @@
 import { Routes } from '@angular/router';
 
+import { PublicLayoutComponent } from '@core/layouts/public-layout/public-layout.component';
+
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
-  },
-  {
-    path: 'schedule',
-    loadComponent: () =>
-      import('./pages/schedule/schedule.component').then((m) => m.ScheduleComponent),
+    component: PublicLayoutComponent,
+    children: [
+      {
+        path: '',
+        data: {
+          preload: true,
+          animation: 'Home',
+          seo: {
+            title: 'Home | TrollySix',
+            description: 'Welcome to ultimate schedule for trolleybus route number 6.',
+            keywords: 'trolleybus, route 6, schedule',
+          },
+        },
+        loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+      },
+      {
+        path: 'schedule',
+        data: {
+          preload: true,
+          animation: 'Home',
+          seo: {
+            title: 'Schedule | TrollySix',
+            description: 'Ultimate schedule for trolleybus route 6.',
+            keywords: 'trolleybus, route 6, schedule',
+          },
+        },
+        loadComponent: () =>
+          import('./pages/schedule/schedule.component').then((m) => m.ScheduleComponent),
+      },
+    ],
   },
   {
     path: '**',
