2980f9b467c4d725b03b674d9549c5fcd91abd9f
Anton Sizov <raindropua@gmail.com>
2025-08-24 15:36:22 +0300

    chore: create sections for the About page with templates and OnPush change detection strategy

---
+++ src/app/pages/about/sections/head/head.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head',
+  imports: [],
+  templateUrl: './head.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeadComponent {
+
+}
