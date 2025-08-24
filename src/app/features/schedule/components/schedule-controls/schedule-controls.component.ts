7ad80854670d16faf44b24373980c418794b18a6
Anton Sizov <raindropua@gmail.com>
2025-08-24 22:49:12 +0300

    chore: refactor ScheduleControls to use structured rendering for departures, update legend and templates with dynamic class-based styles

--- src/app/features/schedule/components/schedule-controls/schedule-controls.component.ts
+++ src/app/features/schedule/components/schedule-controls/schedule-controls.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Departure, Status } from './departure.model';
 
 @Component({
   selector: 'app-schedule-controls',
@@ -6,4 +7,96 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './schedule-controls.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScheduleControlsComponent {}
+export class ScheduleControlsComponent {
+  protected readonly departures: Departure[] = [
+    {
+      status: Status.Past,
+      time: '6:00',
+    },
+    {
+      status: Status.Past,
+      time: '6:26',
+    },
+    {
+      status: Status.Past,
+      time: '6:52',
+    },
+    {
+      status: Status.Past,
+      time: '7:18',
+    },
+    {
+      status: Status.Past,
+      time: '7:45',
+    },
+    {
+      status: Status.Now,
+      time: '8:10',
+    },
+    {
+      status: Status.Soon,
+      time: '8:38',
+    },
+    {
+      status: Status.Coming,
+      time: '9:04',
+    },
+    {
+      status: Status.Coming,
+      time: '9:31',
+    },
+    {
+      status: Status.Coming,
+      time: '10:43',
+    },
+    {
+      status: Status.Coming,
+      time: '11:10',
+    },
+    {
+      status: Status.Coming,
+      time: '11:36',
+    },
+    {
+      status: Status.Coming,
+      time: '12:02',
+    },
+    {
+      status: Status.Coming,
+      time: '12:28',
+    },
+    {
+      status: Status.Coming,
+      time: '12:54',
+    },
+    {
+      status: Status.Coming,
+      time: '13:20',
+    },
+    {
+      status: Status.Coming,
+      time: '13:46',
+    },
+    {
+      status: Status.Coming,
+      time: '14:12',
+    },
+  ];
+}
+
+/*
+14:38
+15:04
+15:30
+15:56
+16:22
+16:48
+17:14
+17:41
+18:08
+18:35
+19:02
+19:25
+19:52
+21:07
+ */
