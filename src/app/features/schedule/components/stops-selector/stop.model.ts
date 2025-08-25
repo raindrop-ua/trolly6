36e66a4f69c2a83bf64f6252ed263ceea60b7820
Anton Sizov <raindropua@gmail.com>
2025-08-25 19:12:16 +0300

    chore: update Stop model to include firstDeparture and lastDeparture attributes

--- src/app/features/schedule/components/stops-selector/stop.model.ts
+++ src/app/features/schedule/components/stops-selector/stop.model.ts
@@ -4,6 +4,8 @@ export interface Stop {
   id: string;
   name: string;
   description: string;
+  firstDeparture: string;
+  lastDeparture: string;
   style: number;
   availableDirections: Directions[];
 }
