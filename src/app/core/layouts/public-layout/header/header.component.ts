9851fd6197e012b4dc8e4482962ee485adaddfb7
Anton Sizov <raindropua@gmail.com>
2025-08-25 14:19:28 +0300

    chore: update the header to track NavigationEnd events, ensure OnPush change detection and proper cleanup

--- src/app/core/layouts/public-layout/header/header.component.ts
+++ src/app/core/layouts/public-layout/header/header.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { NAVIGATION_TOKEN } from '@core/config/navigation.config';
 import { ThemeSwitcherComponent } from '@components/theme-switcher/theme-switcher.component';
 import { AppRouteEnum } from '@core/enums/app-route.enum';
+import { filter } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-header',
@@ -13,10 +15,23 @@ import { AppRouteEnum } from '@core/enums/app-route.enum';
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
+  private cdr = inject(ChangeDetectorRef);
   public navigation = inject(NAVIGATION_TOKEN);
   public readonly showSticky = signal<boolean>(false);
+
+  public ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+        takeUntilDestroyed(this.destroyRef),
+      )
+      .subscribe(() => {
+        this.cdr.markForCheck();
+      });
+  }
 
   public isLinkActive(path: string): boolean {
     return this.router.isActive(path, {
