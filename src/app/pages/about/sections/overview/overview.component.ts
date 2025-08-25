cc33f82b47f26956ef3e8cd0cbec750a5dbd94b1
Anton Sizov <raindropua@gmail.com>
2025-08-25 23:20:33 +0300

    chore: update overview component content

--- src/app/pages/about/sections/overview/overview.component.ts
+++ src/app/pages/about/sections/overview/overview.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LazyVideoDirective } from '@shared/directives/lazy-video.directive';
+import { RevealOnScrollDirective } from '@shared/directives/reveal-on-scroll.directive';
 
 @Component({
   selector: 'app-overview',
   imports: [
-    LazyVideoDirective
+    LazyVideoDirective,
+    RevealOnScrollDirective
   ],
   templateUrl: './overview.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
