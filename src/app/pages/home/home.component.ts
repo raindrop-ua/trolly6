559b38c9819803076d521351ed737951a0cd6068
Anton Sizov <raindropua@gmail.com>
2025-08-25 14:19:14 +0300

    chore: replace DesignedWithPrecision and ItJustWorks with reusable AttentionComponent in a Home section, add dynamic templates for titles, descriptions, and CTAs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,13 @@ import { MastheadComponent } from './sections/masthead/masthead.component';
 import { PromoStripsComponent } from './sections/promo-strips/promo-strips.component';
 import { ValuePropsComponent } from './sections/value-props/value-props.component';
 import { BigClaimsComponent } from './sections/big-claims/big-claims.component';
-import { DesignedWithPrecisionComponent } from './sections/designed-with-precision/designed-with-precision.component';
 import { ThisIsTrollysixComponent } from './sections/this-is-trollysix/this-is-trollysix.component';
-import { ItJustWorksComponent } from './sections/it-just-works/it-just-works.component';
 import { TestimonialsComponent } from './sections/testimonials/testimonials.component';
 import { ReadyForRouteSixComponent } from './sections/ready-for-route-six/ready-for-route-six.component';
 import { MetricsComponent } from '@components/sections/metrics/metrics.component';
 import { Metric } from '@components/sections/metrics/metrics.model';
+import { AttentionComponent } from '@components/sections/attention/attention.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -18,12 +18,12 @@ import { Metric } from '@components/sections/metrics/metrics.model';
     PromoStripsComponent,
     ValuePropsComponent,
     BigClaimsComponent,
-    DesignedWithPrecisionComponent,
     ThisIsTrollysixComponent,
-    ItJustWorksComponent,
     TestimonialsComponent,
     ReadyForRouteSixComponent,
-    MetricsComponent
+    MetricsComponent,
+    AttentionComponent,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
