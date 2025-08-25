db1acb66bf774f7c82faba98fafa06e6007527df
Anton Sizov <raindropua@gmail.com>
2025-08-25 15:11:51 +0300

    chore: integrate SvgIconComponent in stops-selector and enhance template logic for dynamic descriptions

--- src/app/features/schedule/components/stops-selector/stops-selector.component.ts
+++ src/app/features/schedule/components/stops-selector/stops-selector.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component, input, OnInit, signal } from '@angular/core';
 import { Stop } from '@features/schedule/components/stops-selector/stop.model';
+import { SvgIconComponent } from '@components/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-stops-selector',
-  imports: [],
+  imports: [
+    SvgIconComponent
+  ],
   templateUrl: './stops-selector.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -11,6 +14,7 @@ export class StopsSelectorComponent implements OnInit {
   public readonly preselected = input<string>();
   public readonly selectedStopId = signal<string | null>(null);
   public readonly stops = input.required<Stop[]>();
+  public readonly showDescriptions = input<boolean>(true);
 
   ngOnInit() {
     this.selectedStopId.set(this.preselected() ?? this.stops()[0].id);
