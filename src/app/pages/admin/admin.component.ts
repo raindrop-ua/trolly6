261e1b7ed7899f65603c2804d81a436b7434e28f
Anton Sizov <raindropua@gmail.com>
2025-08-26 14:17:19 +0300

    feat: update an admin component to include SVG logo and adjust layout

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SvgIconComponent } from '@components/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [
+    SvgIconComponent
+  ],
   templateUrl: './admin.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
