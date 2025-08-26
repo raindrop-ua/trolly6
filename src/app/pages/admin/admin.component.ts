5ef986a9e5dfdd1d0d72a02fe7b2187989d59736
Anton Sizov <raindropua@gmail.com>
2025-08-26 14:27:45 +0300

    feat: enhance an admin component with a theme switcher and update header layout

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SvgIconComponent } from '@components/svg-icon/svg-icon.component';
+import { ThemeSwitcherComponent } from '@components/theme-switcher/theme-switcher.component';
 
 @Component({
   selector: 'app-admin',
   imports: [
-    SvgIconComponent
+    SvgIconComponent,
+    ThemeSwitcherComponent
   ],
   templateUrl: './admin.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
