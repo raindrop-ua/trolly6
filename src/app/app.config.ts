8281487800f432b23113b9dff05378df6b9c9dc3
Anton Sizov <raindropua@gmail.com>
2025-08-28 16:53:43 +0300

    feat: clean up package.json and app.config.ts by removing unused dependencies and imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
+import { isPlatformBrowser } from '@angular/common';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {
   ApplicationConfig,
+  PLATFORM_ID,
   inject,
   isDevMode,
   provideEnvironmentInitializer,
@@ -10,18 +12,11 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 
-import { InMemoryCache } from '@apollo/client/core';
 import { NAVIGATION, NAVIGATION_TOKEN } from '@core/config/navigation.config';
 import { SeoService } from '@core/services/seo.service';
 import { AfterFirstPaintPreloadingStrategy } from '@core/strategies/after-first-paint-preloading.strategy';
-import { provideEffects } from '@ngrx/effects';
-import { provideRouterStore } from '@ngrx/router-store';
-import { provideStore } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { provideApollo } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
+import { SwUpdateService } from '@services/sw-update.service';
 
-import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -45,35 +40,12 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    provideStore(),
-    provideStoreDevtools({
-      maxAge: 25,
-      autoPause: true,
-      logOnly: !isDevMode(),
-      trace: true,
-      traceLimit: 75,
-    }),
-    provideEffects(),
-    provideRouterStore(),
-    provideHttpClient(),
-    provideApollo(() => {
-      const httpLink = inject(HttpLink);
-
-      return {
-        link: httpLink.create({
-          uri: environment.apiUrl,
-        }),
-        cache: new InMemoryCache(),
-        defaultOptions: {
-          watchQuery: {
-            fetchPolicy: 'cache-and-network',
-            notifyOnNetworkStatusChange: true,
-          },
-          query: {
-            fetchPolicy: 'network-only',
-          },
-        },
-      };
+    provideEnvironmentInitializer(() => {
+      const platformId = inject(PLATFORM_ID);
+      if (isPlatformBrowser(platformId)) {
+        inject(SwUpdateService);
+      }
+      inject(SeoService);
     }),
   ],
 };
